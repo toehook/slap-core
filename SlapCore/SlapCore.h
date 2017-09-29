2e234d91bddd166d20cd844e1ae02fb50d1215ef
Oleksii Dykan <alick_bass@mail.ru>
2017-09-29 17:05:05 +0800

    Initial project setup

---
+++ SlapCore/SlapCore.h
@@ -0,0 +1,19 @@
+//
+//  SlapCore.h
+//  SlapCore
+//
+//  Created by Oleksii on 29/09/2017.
+//  Copyright © 2017 Toehook. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlapCore.
+FOUNDATION_EXPORT double SlapCoreVersionNumber;
+
+//! Project version string for SlapCore.
+FOUNDATION_EXPORT const unsigned char SlapCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlapCore/PublicHeader.h>
+
+
